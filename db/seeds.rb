7b3c6e065a516e0280ac83534c6c71be552e5fcf
Keila Lopez <keilalofra@gmail.com>
2022-11-28 15:19:05 -0600

    commit tree 6a1167828c2d8dc9ab5a260b6ef9d87607751552

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
 # Add a console message so we can see output when the seed file runs
+puts "Seeding games..."
+
+#run a loop 50 times
+50.times do 
+    #create a game with random data
+    Game.create(
+        title: Faker::Game.title,
+        genre: Faker::Game.genre, 
+        platform: Faker::Game.platform,
+        price: rand(0..60) #random number 0-60
+    )
+end
+
+puts "Done seeding!"
